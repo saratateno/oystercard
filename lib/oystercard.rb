669050ecc0859f40377f7ae9ad4e058384539d32
edwardkerry <edwardkerry@gmail.com>
2015-11-30 17:13:11 +0000

    Initial balance set at 0, basic top_up test

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,2 +1,13 @@
 class Oystercard
+
+  attr_reader :balance
+
+  def initialize
+    @balance = 0
+  end
+
+  def top_up(amount)
+    @balance += 10
+  end
+
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,4 +2,18 @@ require 'oystercard'
 
 describe Oystercard do
 
+subject(:card) {described_class.new}
+
+  it 'should have a balance of 0' do
+    expect(subject.balance).to eq 0
+  end
+
+  describe '#top_up' do
+    it 'should increase the balance by 10 after topping up with 10' do
+        card.top_up(10)
+        expect(card.balance).to eq 10
+    end
+  end
+
+
 end
