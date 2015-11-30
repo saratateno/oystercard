d14cbdc3a0a86d521d342ba3f408f1710c872e35
Sara Tateno <sara.tateno@gmail.com>
2015-11-30 21:57:52 +0000

    Set balance limit for top up. Created deduct method

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,16 +1,18 @@
 class Oystercard
-<<<<<<< HEAD
-=======
 
   attr_reader :balance
+  BALANCE_LIMIT = 90
 
   def initialize
     @balance = 0
   end
 
   def top_up(amount)
+    fail "Maximum value of £#{BALANCE_LIMIT} exceeded" if balance + amount > BALANCE_LIMIT
     @balance += amount
   end
 
->>>>>>> Day1
+  def deduct(amount)
+    @balance -= amount
+  end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,25 +1,27 @@
 require 'oystercard'
 
 describe Oystercard do
-
-subject(:card) {described_class.new}
+  subject(:card) {described_class.new}
 
   it 'should have a balance of 0' do
     expect(subject.balance).to eq 0
   end
 
   describe '#top_up' do
-    it 'should increase the balance by 10 after topping up with 10' do
-        card.top_up(10)
-        expect(card.balance).to eq 10
+    it 'can top up the balance' do
+      expect{ card.top_up 1 }.to change{ card.balance }.by 1
     end
 
-    it 'should increase the existing balance by an amount' do
-        card.top_up(10)
-        card.top_up(5)
-        expect(card.balance).to eq 15
+    it 'raises an error if the BALANCE_LIMIT is exceeded' do
+      maximum_value = Oystercard::BALANCE_LIMIT
+      card.top_up(maximum_value)
+      expect{ card.top_up 1 }.to raise_error("Maximum value of £#{maximum_value} exceeded")
     end
-
   end
 
+  describe '#deduct' do
+    it 'reduces the balance' do
+      expect{ card.deduct 1 }.to change{ card.balance }.by -1
+    end
+  end
 end
