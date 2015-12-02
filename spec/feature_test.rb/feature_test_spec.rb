b678011351947658cd60378c032d7166d6d743d1
Sara Tateno <sara.tateno@gmail.com>
2015-12-02 16:23:28 +0000

    Created Station class

--- spec/feature_test.rb/feature_test_spec.rb
+++ spec/feature_test.rb/feature_test_spec.rb
@@ -3,9 +3,9 @@ describe "Features Tests" do
 let(:oystercard) {Oystercard.new}
 let(:maximum_balance) {Oystercard::MAXIMUM_BALANCE}
 let(:minimum_fare) {Oystercard::MINIMUM_FARE}
-let(:station) {double :station}
 let(:in_station) {double :station}
 let(:out_station) {double :station}
+let(:station) {Station.new('Shoreditch',1)}
 
 
   it 'creates a new oystercard with a balance of 0' do
@@ -23,23 +23,23 @@ let(:out_station) {double :station}
    end
 
   it 'prevents touching in if insufficient funds' do
-    expect{oystercard.touch_in(station)}.to raise_error 'Insufficient funds: top up'
+    expect{oystercard.touch_in(in_station)}.to raise_error 'Insufficient funds: top up'
   end
 
   it 'deducts fare from oystercard' do
-    expect{oystercard.touch_out(station)}.to change{oystercard.balance}.by(-minimum_fare)
+    expect{oystercard.touch_out(out_station)}.to change{oystercard.balance}.by(-minimum_fare)
   end
 
   it 'remembers the entry station after touch in' do
     oystercard.top_up(minimum_fare)
-    oystercard.touch_in(station)
-    expect(oystercard.entry_station).to eq station
+    oystercard.touch_in(in_station)
+    expect(oystercard.entry_station).to eq in_station
   end
 
   it 'forgets the entry station on touch out' do
     oystercard.top_up(minimum_fare)
-    oystercard.touch_in(station)
-    oystercard.touch_out(station)
+    oystercard.touch_in(in_station)
+    oystercard.touch_out(out_station)
     expect(oystercard.entry_station).to eq nil
   end
 
@@ -54,14 +54,12 @@ let(:out_station) {double :station}
   it 'starts with an empty journey history' do
      expect(oystercard.history).to be_empty
   end
-  #
-  # it 'stores multiple journeys' do
-  #   2.times do
-  #     oystercard.top_up(minimum_fare)
-  #     oystercard.touch_in(in_station)
-  #     oystercard.touch_out(out_station)
-  #   end
-  #   expect(oystercard.history).to eq
-  # end
 
+  it 'creates a new station and sets its name as an instance variable' do
+    expect(station.name).to eq 'Shoreditch'
+  end
+
+  it 'creates a new station and sets its zone as an instance variable' do
+    expect(station.zone).to eq 1
+  end
 end
